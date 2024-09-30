095d4728365ed5b1e18342577128781fbded4b7d
Tom Price <tp@palletonline.co.uk>
2024-09-30 16:56:44 +0100

    add heros and data to all pages

--- src/app/services/emergency-lighting/page.tsx
+++ src/app/services/emergency-lighting/page.tsx
@@ -1,8 +1,11 @@
 import UnderConstruction from "@/components/layout/UnderConstruction";
+import HeroUnit from "@/components/blocks/hero/HeroUnit";
+import data from "./page.json";
 
 export default function Page() {
   return (
     <div>
+      <HeroUnit {...data.hero} />
       <UnderConstruction />
     </div>
   );
--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,8 +1,11 @@
+import HeroUnit from "@/components/blocks/hero/HeroUnit";
 import UnderConstruction from "@/components/layout/UnderConstruction";
+import data from "./page.json";
 
 export default function Page() {
   return (
     <div>
+      <HeroUnit {...data.hero} />
       <UnderConstruction />
     </div>
   );
