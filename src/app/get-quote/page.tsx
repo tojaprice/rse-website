67f9651428add4c6e3c9de107c4553dddd7c28ab
Tom Price <tp@palletonline.co.uk>
2024-10-28 11:55:11 +0000

    get quote hero

--- src/app/get-quote/page.tsx
+++ src/app/get-quote/page.tsx
@@ -1,8 +1,11 @@
+import HeroUnit from "@/components/blocks/hero/HeroUnit";
 import UnderConstruction from "@/components/layout/UnderConstruction";
+import data from "./page.json";
 
 export default function GetQuote() {
   return (
     <div>
+      <HeroUnit {...data.hero} />
       <UnderConstruction />
     </div>
   );
